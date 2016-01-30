698b53dc99d664f518c6c35b4158c1bb58633069
Emily Randall <emilymrandall@comcast.net>
2016-01-30 15:12:33 -0700

    Week 3 assignment cleanup

--- src/apps/resume/js/main.js
+++ src/apps/resume/js/main.js
@@ -1,25 +1,25 @@
-$(document).ready(function() { 
-  showPage("welcome");
-  
-  $('.parallax').parallax();
-  
-  $("#Resume").click(function(){
-    showPage('resume');
-  });
-  $("#Tasks").click(function(){
-    showPage('tasks');
-  });
-  $("#Cities").click(function(){
-    showPage('cities', function() {
-      initMap();
-    });
-  });
-  $("#Home").click(function(){
-    showPage('welcome');
-  });
-  
+$(document).ready(function() {
+  if (window.location.hash === '') {
+    showPage("welcome");
+  }
+  else {
+    hashChange();
+  }
 });
+
+$(window).bind('hashchange', hashChange);
 
 function showPage(file, completedFunc) {
   $( "#presentation_content" ).load( "./pages/" + file+".html", completedFunc);
+}
+
+function hashChange() {
+  if (window.location.hash === '#cities') {
+    showPage('cities', function() {
+      initMap();
+    });
+  }
+  else {
+    showPage(window.location.hash.slice(1));
+  }
 }
