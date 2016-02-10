a03ec7d96dc05e398511a867732deaa857996cae
Emily Randall <emilymrandall@comcast.net>
2016-02-10 00:33:20 -0700

    Creating landing page

--- src/apps/uber/components/navbar.jsx
+++ src/apps/uber/components/navbar.jsx
@@ -4,7 +4,7 @@ class NavBar extends React.Component {
     return (
       <nav>
         <div className="nav-wrapper blue darken-4">
-        <a href="../index.html" className="brand-logo">Home</a>
+        <a href="../index.html" className="brand-logo">RideSki</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li><a href="../client/drivers.html">Drivers</a></li>
           <li><a href="../client/riders.html">Riders</a></li>
