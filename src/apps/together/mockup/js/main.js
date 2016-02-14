73b3c54568e3aa8ab7678e063cb1c0d1298a2be3
Emily Randall <emilymrandall@comcast.net>
2016-02-14 10:42:29 -0700

    upgrades to mockup

--- src/apps/together/mockup/js/main.js
+++ src/apps/together/mockup/js/main.js
@@ -15,4 +15,17 @@ function showPage(file, completedFunc) {
 
 function hashChange() {
   showPage(window.location.hash.slice(1));
+  console.log(getURLParameter('key'));
+}
+
+function getURLParameter(sParam) {
+  var sPageURL = window.location.search.substring(1);
+  var sURLVariables = sPageURL.split('&');
+  for (var i = 0; i < sURLVariables.length; i++) {
+    var sParameterName = sURLVariables[i].split('=');
+    if (sParameterName[0] == sParam) {
+      return sParameterName[1];
+    }
+  }
+  return "";
 }
